4c39a99eceb48b57b3721dfb228f5128b5213abc
Hari Vignesh A <142196988+BEASTY2K3@users.noreply.github.com>
2025-10-14 15:41:11 +0530

    fix:image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@
  */
 const nextConfig = {
   output: 'export',
+  images:{
   domains: ['static.mycdn.com', 'images.example.com'],
   unoptimized:true,
- 
-}
+  },
+};
  
 module.exports = nextConfig
